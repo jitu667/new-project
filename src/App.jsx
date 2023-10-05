f1e6d03187ea0d376c6815608c38dd43e3ca2ffe
Jitu koli <138875642+jitu667@users.noreply.github.com>
2023-10-05 15:37:35 +0530

    undo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import introImg from './src/assets/image-intro-desktop.jpg'
 import './App.css'
 
 function App() {
