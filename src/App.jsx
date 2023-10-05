5850da5279006d3bc5ae8fcd8806bec02a343c2a
Jitu koli <138875642+jitu667@users.noreply.github.com>
2023-10-05 15:42:07 +0530

    path change

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <>
       {/* Navbar Start */}
@@ -40,7 +39,7 @@ function App() {
 
       {/* First Part Start */}
       <div className="intro-img-mob">
-        <img src="..\src\assets\image-intro-mobile.jpg" alt="" />
+        <img src="..\new-project\src\assets\image-intro-mobile.jpg" alt="" />
       </div>
       <div className="container-fluid">
         <div className="container d-flax bg-transparent">
@@ -54,7 +53,7 @@ function App() {
               <button className="view-plans-button mt-5" type="submit">view plans</button>
             </div>
             <div className="col intro-img">
-              <img src="..\src\assets\image-intro-desktop.jpg" alt="" />
+              <img src="..\new-project\src\assets\image-intro-desktop.jpg" alt="" />
             </div>
           </div>
         </div>
